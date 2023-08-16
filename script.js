bd76312df29166003089629a2aaee438498a9d33
AndersonSantosDias <andersonSD.dev@gmail.com>
2023-08-16 14:45:40 -0300

    simplificação de codigo v0.2

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ function calcular(){
     let ultima = document.getElementById('ultima')
     let base = [30, 30, 5, 45, 20, 4, 20, 30, 7, 20, 25, 8, 30, 35, 6, 40, 25, 4, 25, 35, 5, 40, 15, 6, 20, 40, 5, 45, 20, 4]
 
-    /* Calculo das raças */
     if(raca.value == "Humano"){dados(base[0], base[1], base[2])}
     if(raca.value == "Vagariano"){dados(base[3], base[4], base[5]); defesa += Number(constituicao.value)/5}
     if(raca.value == "Naniki"){dados(base[6], base[7], base[8])}
@@ -35,7 +34,27 @@ function calcular(){
     if(raca.value == "Tsukakuma"){dados(base[24], base[25], base[26]); PV += PM * 0.05; dfis += Number(inteligencia.value) * 0.2}
     if(raca.value == "Anão"){dados(base[27], base[28], base[29]); PV += Number(constituicao.value) * 5}
 
-    /* Alteração no HTML */
+    function dados(p, e, d){
+        if (Number(primaria.value) == 1){PM = (e * 1.5) * Number(niv.value) + Number(magia.value) * 30; dmag = Number(inteligencia.value)} else{PM = e * Number(niv.value) + Number(magia.value) * 20}
+        if (Number(primaria.value) == 2){PV = p * Number(niv.value) + Number(constituicao.value) * 15; defesa = Number(constituicao.value)/5; dfis = Number(forca.value)} else{PV = p * Number(niv.value) + Number(constituicao.value) * 10; defesa = Number(constituicao.value)/10}
+        if (Number(primaria.value) == 3){dfisarm = Number(forca.value)/2 + Number(destreza.value)/2}
+        if (Number(primaria.value) == 4){desp = Number(inteligencia.value)}
+        if (Number(secundaria.value) == 1){dmag = Number(inteligencia.value) * 0.8}
+        if (Number(secundaria.value) == 2){dfis = Number(forca.value) * 0.8}
+        if (Number(secundaria.value) == 3){dfisarm = (Number(forca.value)/2 + Number(destreza.value)/2) * 0.8}
+        if (Number(secundaria.value) == 4){desp = Number(inteligencia.value) * 0.8}
+        if (Number(tercearia.value) == 1){dmag = Number(inteligencia.value) * 0.5}
+        if (Number(tercearia.value) == 2){dfis = Number(forca.value) * 0.5}
+        if (Number(tercearia.value) == 3){dfisarm = (Number(forca.value)/2 + Number(destreza.value)/2) * 0.5}
+        if (Number(tercearia.value) == 4){desp = Number(inteligencia.value) * 0.5}
+        if (Number(ultima.value) == 1){dmag = Number(inteligencia.value) * 0.4}
+        if (Number(ultima.value) == 2){dfis = Number(forca.value) * 0.4}
+        if (Number(ultima.value) == 3){dfisarm = (Number(forca.value)/2 + Number(destreza.value)/2) * 0.4}
+        if (Number(ultima.value) == 4){desp = Number(inteligencia.value) * 0.4}
+        deslocamento = d + Number(velocidade.value)/20
+        ações = 3 + Number(destreza.value)/20
+    }
+    
     let resultado = document.getElementById('res');
     let res = `
     Raça: ${raca.value} <br>
@@ -48,78 +67,11 @@ function calcular(){
     Dano armado: ${dfisarm.toFixed(1)} <br>
     Dano magico: ${dmag} <br>
     Dano espiritual: ${desp.toFixed(1)} <br>`;
-
     let paragrafo = document.querySelector('#res p')
     if (paragrafo) {paragrafo.remove()}
     let novo = document.createElement('p')
     novo.innerHTML = res
     resultado.appendChild(novo)
-
-    /* funções extras */
-    function dados(p, e, d){
-        if (Number(primaria.value) == 1){
-            PM = (e * 1.5) * Number(niv.value) + Number(magia.value) * 30
-            dmag = Number(inteligencia.value)
-        } else{
-            PM = e * Number(niv.value) + Number(magia.value) * 20
-        }
-        if (Number(primaria.value) == 2){
-            PV = p * Number(niv.value) + Number(constituicao.value) * 15
-            defesa = Number(constituicao.value)/5
-            dfis = Number(forca.value)
-        } else{
-            PV = p * Number(niv.value) + Number(constituicao.value) * 10
-            defesa = Number(constituicao.value)/10
-        }
-        if (Number(primaria.value) == 3){
-            dfisarm = Number(forca.value)/2 + Number(destreza.value)/2
-        }
-        if (Number(primaria.value) == 4){
-            desp = Number(inteligencia.value)
-        }
-    
-        if (Number(secundaria.value) == 1){
-            dmag = Number(inteligencia.value) * 0.8
-        }
-        if (Number(secundaria.value) == 2){
-            dfis = Number(forca.value) * 0.8
-        }
-        if (Number(secundaria.value) == 3){
-            dfisarm = (Number(forca.value)/2 + Number(destreza.value)/2) * 0.8
-        }
-        if (Number(secundaria.value) == 4){
-            desp = Number(inteligencia.value) * 0.8
-        }
-    
-        if (Number(tercearia.value) == 1){
-            dmag = Number(inteligencia.value) * 0.5
-        }
-        if (Number(tercearia.value) == 2){
-            dfis = Number(forca.value) * 0.5
-        }
-        if (Number(tercearia.value) == 3){
-            dfisarm = (Number(forca.value)/2 + Number(destreza.value)/2) * 0.5
-        }
-        if (Number(tercearia.value) == 4){
-            desp = Number(inteligencia.value) * 0.5
-        }
-    
-        if (Number(ultima.value) == 1){
-            dmag = Number(inteligencia.value) * 0.4
-        }
-        if (Number(ultima.value) == 2){
-            dfis = Number(forca.value) * 0.4
-        }
-        if (Number(ultima.value) == 3){
-            dfisarm = (Number(forca.value)/2 + Number(destreza.value)/2) * 0.4
-        }
-        if (Number(ultima.value) == 4){
-            desp = Number(inteligencia.value) * 0.4
-        }
-    
-        deslocamento = d + Number(velocidade.value)/20
-        ações = 3 + Number(destreza.value)/20
-    }
 }
 function opção1(){
     let primeira = document.getElementById('primaria')
